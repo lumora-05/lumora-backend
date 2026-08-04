dc74be1c46c72609929da17c541870419c1f272d
huynhpv <lalong99x@gmail.com>
2026-08-04 22:04:57 +0700

    update múi giờ

--- src/main/java/com/example/restaurant/RestaurantBackendApplication.java
+++ src/main/java/com/example/restaurant/RestaurantBackendApplication.java
@@ -3,9 +3,12 @@ package com.example.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class RestaurantBackendApplication {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
         SpringApplication.run(RestaurantBackendApplication.class, args);
     }
 }
